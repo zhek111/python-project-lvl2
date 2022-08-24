8508404c34c5f4cd7a882d2f85d222c091a90e9f
evgeny <zhek12322@gmail.com>
2022-08-24 16:59:00 +0300

    fix data.py

--- gendiff/moduls/data.py
+++ gendiff/moduls/data.py
@@ -8,7 +8,7 @@ def get_data(path_file1, path_file2):
     if path_file1.endswith('.json'):
         file1 = json.load(open(path_file1))
     if path_file2.endswith('.yaml') or path_file2.endswith('.yml'):
-        file2 = json.load(open(path_file2))
+        file2 = yaml.safe_load(open(path_file2))
     if path_file2.endswith('.json'):
         file2 = json.load(open(path_file2))
     return file1, file2
