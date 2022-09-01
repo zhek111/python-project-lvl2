39e9ab041191d77e3814f36bc59dbe886e8a7bb5
evgeny <zhek12322@gmail.com>
2022-09-01 13:42:53 +0300

    add parametrize

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,12 +1,27 @@
+import pytest as pytest
+
 from gendiff import generate_diff
+import pytest
 
 
-def test_generare_diff_json():
+@pytest.mark.parametrize("test_input1,test_input2, expected", [(
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+        'tests/fixtures/correct_result.txt'), (
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+        'tests/fixtures/correct_result.txt')])
+def test_generare_diff_json(test_input1, test_input2, expected):
+    with open(expected, 'r') as file:
+        result_data = file.read()
+    assert generate_diff(test_input1, test_input2) == result_data
+
+
+def test_generare_diff_json1():
     with open('tests/fixtures/correct_result.txt', 'r') as file:
         result_data = file.read()
     assert generate_diff('tests/fixtures/file1.json',
                          'tests/fixtures/file2.json') == result_data
-
 
 def test_generare_diff_yaml():
     with open('tests/fixtures/correct_result.txt', 'r') as file:
@@ -28,21 +43,21 @@ def test_generare_diff_empty():
 
 
 def test_generare_diff_tree_json():
-    with open('tests/fixtures/correct_result_tree.rtf', 'r') as file:
+    with open('tests/fixtures/correct_result_tree.txt', 'r') as file:
         result_data = file.read()
     assert generate_diff('tests/fixtures/file1_tree.json',
                          'tests/fixtures/file2_tree.json') == result_data
 
 
 def test_generare_diff_tree_yaml():
-    with open('tests/fixtures/correct_result_tree.rtf', 'r') as file:
+    with open('tests/fixtures/correct_result_tree.txt', 'r') as file:
         result_data = file.read()
     assert generate_diff('tests/fixtures/file1_tree.yaml',
                          'tests/fixtures/file2_tree.yaml') == result_data
 
 
 def test_generare_diff_tree_plain():
-    with open('tests/fixtures/correct_result_tree_plain.rtf', 'r') as file:
+    with open('tests/fixtures/correct_result_tree_plain.txt', 'r') as file:
         result_data = file.read()
     assert generate_diff('tests/fixtures/file1_tree.yaml',
                          'tests/fixtures/file2_tree.yaml',
@@ -50,7 +65,7 @@ def test_generare_diff_tree_plain():
 
 
 def test_generare_diff_tree_get_json():
-    with open('tests/fixtures/correct_result_tree_json.rtf', 'r') as file:
+    with open('tests/fixtures/correct_result_tree_json.txt', 'r') as file:
         result_data = file.read()
     assert generate_diff('tests/fixtures/file1_tree.json',
                          'tests/fixtures/file2_tree.json',
