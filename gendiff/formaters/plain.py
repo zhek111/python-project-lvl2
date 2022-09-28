6df24c89bf4284a3bdbe68dc0941e119912fc207
evgeny <zhek12322@gmail.com>
2022-09-28 20:12:16 +0300

    fix2 comment2 in project

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,8 +1,5 @@
 from typing import Any
 
-DEFAULT_DEPTH = list()
-
-
 def determine_value(dictionary: dict, value: Any) -> None:
     if isinstance(dictionary[value], dict):
         dictionary[value] = "[complex value]"
@@ -18,26 +15,26 @@ def determine_value(dictionary: dict, value: Any) -> None:
 
 def build_plain_iter(diff: list[dict], depth: list = None) -> str:
     if depth is None:
-        depth = DEFAULT_DEPTH
-    text = list()
+        depth = list()
+    list_str = list()
     for dictionary in diff:
         property = '.'.join(depth + [dictionary['key']])
         if dictionary['operation'] == 'add':
             determine_value(dictionary, 'new')
-            text.append(f"Property '{property}' "
+            list_str.append(f"Property '{property}' "
                         f"was added with value: {dictionary['new']}")
         if dictionary['operation'] == 'removed':
-            text.append(f"Property '{property}' was removed")
+            list_str.append(f"Property '{property}' was removed")
         if dictionary['operation'] == 'nested':
             new_value = build_plain_iter(dictionary['value'],
                                          depth + [dictionary['key']])
-            text.append(f"{new_value}")
+            list_str.append(f"{new_value}")
         if dictionary['operation'] == 'changed':
             determine_value(dictionary, 'new')
             determine_value(dictionary, 'old')
-            text.append(f"Property '{property}' was updated. "
+            list_str.append(f"Property '{property}' was updated. "
                         f"From {dictionary['old']} to {dictionary['new']}")
-    text_str = '\n'.join(text)
+    text_str = '\n'.join(list_str)
     return text_str
 
 
